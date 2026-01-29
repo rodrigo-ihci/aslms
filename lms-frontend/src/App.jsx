e6e91c9fb143fed61e09bfe1c0724652cda9fe70
Rodrigo <rodrigo.aguilera@ihci.edu.hn>
2026-01-29 09:45:28 -0600

    add: lms-frontend (Hello World print)

---
+++ lms-frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+const App = () => {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+	<div>
+	    Hello World
+      </div>
+    </>
+  )
+}
+
+export default App
